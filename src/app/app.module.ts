2564e518179da9e599e26733270f4a8c5d39a25c
Ajla Zulović <ajlazulovic55@gmail.com>
2023-12-02 22:00:55 +0100

    changes1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TextComponent } from './text/text.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TextComponent,
+    NavbarComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
