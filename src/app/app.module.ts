09c16a83715249f1801995c11a3ac314fb2c3862
Ajla Zulović <ajlazulovic55@gmail.com>
2024-01-05 16:00:27 +0100

    uređivanjeKoda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TextComponent } from './text/text.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { InputComponent } from './input/input.component';
-import { RfiltrationComponent } from './rfiltration/rfiltration.component';
 import { FormsModule } from '@angular/forms';
-import { OffersComponent } from './offers/offers.component';
 import{HttpClientModule} from '@angular/common/http';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
@@ -26,11 +21,6 @@ const appRoute:Routes=[
   declarations: [
     AppComponent,
     HeaderComponent,
-    TextComponent,
-    NavbarComponent,
-    InputComponent,
-    RfiltrationComponent,
-    OffersComponent,
     MainComponent,
     AboutComponent,
     HomeComponent,
