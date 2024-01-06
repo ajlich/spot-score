ab887d0bf2d686d7fb4ea29a06ef3636d0c04d1e
Ajla Zulović <ajlazulovic55@gmail.com>
2024-01-07 00:08:06 +0100

    organizing details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,7 +27,72 @@ export class HomeComponent {
       img:'assets/mirazur.jpg',
       rating:'stars',
       type:'restaurant'
+    },
+    {  
+      id:4,
+      name:'Four Seasons Firenze',
+      img:'assets/hotel1.jpg',
+      rating:'stars',
+      type:'hotel'
+    },
+    {  
+      id:5,
+      name:'Soneva Fushi',
+      img:'assets/hotel2.jpg',
+      rating:'stars',
+      type:'hotel'
+    },
+    {  
+      id:6,
+      name:'Raffles Singapore',
+      img:'assets/hotel3.jpg',
+      rating:'stars',
+      type:'hotel'
+    },
+    {  
+      id:7,
+      name:'Don Quijote',
+      img:'assets/don.jpg',
+      rating:'stars',
+      type:'book'
+    },
+    {  
+      id:8,
+      name:'The Lord of the Rings',
+      img:'assets/rings.jpg',
+      rating:'stars',
+      type:'book'
+    },
+    {  
+      id:9,
+      name:'The Great Gatsby',
+      img:'assets/gatsby.jpg',
+      rating:'stars',
+      type:'book'
+    },
+    {  
+      id:10,
+      name:'Avatar',
+      img:'assets/avatar.jpg',
+      rating:'stars',
+      type:'movie'
+    },
+    {  
+      id:11,
+      name:'Titanic',
+      img:'assets/titanic.jpg',
+      rating:'stars',
+      type:'movie'
+    },
+    {  
+      id:12,
+      name:'Captain America: Civil War',
+      img:'assets/america.jpg',
+      rating:'stars',
+      type:'movie'
     }
 ]
+
+
 selectedOption:string="Best";
 }
