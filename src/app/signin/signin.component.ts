692c8ac78906d9efd10a706fcd4fc2b74f913594
Ajla Zulović <ajlazulovic55@gmail.com>
2024-01-18 00:01:17 +0100

    Password Validation

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -11,11 +11,20 @@ export class SigninComponent implements OnInit {
   ngOnInit(){
     this.reactiveForm=new FormGroup({
       userName:new FormControl(null,Validators.required),
-      email:new FormControl(null),
-      password:new FormControl(null)
+      email:new FormControl(null,[Validators.required,Validators.email]),
+      password:new FormControl(null,[Validators.required,this.passwordValidator])
     });
   }
   onSumbit(){
     console.log(this.reactiveForm);
+  }
+  passwordValidator(control: any): { [key: string]: boolean } | null {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,10}$/;
+
+    if (!passwordRegex.test(control.value)) {
+      return { 'invalidPassword': true };
+    }
+
+    return null;
   }
 }
